8bee6363a4e73feb99da66ad98c133acdada0b3f
Patti Vacek <pattivacek@gmail.com>
2018-01-12 15:21:28 +0100

    Add nm-applet control.

--- awesome_rc.lua
+++ awesome_rc.lua
@@ -554,5 +554,7 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
-awful.util.spawn_with_shell("xscreensaver -no-splash")
+awful.util.spawn_with_shell("pgrep -u $USER -x xscreensaver > /dev/null || (xscreensaver -no-splash &)")
+-- https://askubuntu.com/a/183334/173694
+awful.util.spawn_with_shell("pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)")
 
