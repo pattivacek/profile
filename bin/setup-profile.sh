c5f4116220459b0553aa78bec285e41deb2c98f8
Patti Vacek <pattivacek@gmail.com>
2021-06-11 11:11:14 +0200

    Setup git-praise.
    Also, safer installation (backup existing files) and add some TODOs.
    
    Signed-off-by: Patti Vacek <pattivacek@gmail.com>

--- bin/setup-profile.sh
+++ bin/setup-profile.sh
@@ -6,9 +6,14 @@ cd ~
 mkdir -p bin
 cd bin
 ln -s ~/Code/profile/bin/termtitle.sh .
+# TODO: make sure git-praise is present, and/or fetch it.
+# TODO: make sure ruby is installed.
+ln -s ~/Code/git-praise/git-praise .
 cd ..
 
-rm -f .bashrc .profile
+mv ~/.bashrc ~/.bashrc-backup
+mv ~/.profile ~/.profile-backup
+
 ln -s ~/Code/profile/bashrc .bashrc
 ln -s ~/Code/profile/gitconfig .gitconfig
 ln -s ~/Code/profile/profile .profile
@@ -18,3 +23,5 @@ ln -s ~/Code/profile/vimrc .vimrc
 cd .vim
 git submodule update --init --recursive
 cd ..
+
+# TODO: make sure vim is installed.
